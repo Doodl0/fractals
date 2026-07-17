2787ad7a37aaf50f1c7522eb494f141509f8dab0
Doodl0 <52604696+Doodl0@users.noreply.github.com>
2026-07-17 07:44:19 +0100

    Allowed zooming into Mandelbrot with arrow keys and no longer calculate brand new set every frame, only when zoom changes

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include "SDL3/SDL_events.h"
+#include "SDL3/SDL_oldnames.h"
 #include "SDL3/SDL_pixels.h"
 #include "SDL3/SDL_render.h"
 #include "SDL3/SDL_scancode.h"
 #include "SDL3/SDL_surface.h"
+#include "SDL3/SDL_thread.h"
+#include "SDL3/SDL_timer.h"
 #include <stddef.h>
 #define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
@@ -15,6 +18,23 @@
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+static SDL_Surface *surface = NULL;
+
+float last_zoom = 0.0f;
+
+typedef struct {
+    int x;
+    int y;
+} Pixel;
+
+// hardcoded max data because i'm stupid
+typedef struct {
+    int width;
+    int height;
+    int start_x;
+    int start_y;
+    SDL_Color pixel_data[X_RESOLUTION][Y_RESOLUTION];
+} ChunkData;
 
 // Hopefully allows the fractals to be dynamically zoomed into and out of
 float zoom = 1.0f;
@@ -29,42 +49,38 @@ float input_zoom(const SDL_Event *event, float zoom) {
 }
 
 // Mandelbrot function shamelessly copied from wikipedia
-SDL_Color mandelbrot(int Px, int Py) {
+int Mandelbrot(int Px, int Py) {
     // Scaled x coordinate of pixel (scaled to lie in the Mandelbrot X scale (-2.00, 0.47))
-    float x0 = ((float)Px / (float)X_RESOLUTION) * 2.47f - 2.0f;
+    float x0 = (((float)Px / (float)X_RESOLUTION) * 2.47f - 2.0f) * zoom;
     // Scaled y coordinare of pixel (scaled to lie in the Mandelbrot Y scale (-1.12, 1.12))
-    float y0 = ((float)Py / (float)Y_RESOLUTION) * 2.24f - 1.0f;
+    float y0 = ((float)Py / ((float)Y_RESOLUTION) * 2.24f - 1.0f) * zoom;
 
-    float x = 0;
-    float y = 0;
+    float x, y = 0;
     int iteration = 0;
-    int max_iteration = 1000;
+    int max_iteration = 500;
 
-    float x2 = 0;
-    float y2 = 0;
-    float w = 0;
-
-    while (x2 + y2 <= 4 && iteration < max_iteration) {
-        x = x2 - y2 + x0;
-        y = w - x2 - y2 + y0;
-        x2 = x * x;
-        y2 = y * y;
-        w = (x + y) * (x + y);
+    while (x*x + y*y <= ( 1 << 16) && iteration < max_iteration) {
+        float xtemp = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = xtemp;
         iteration += 1;
     }
 
-    SDL_Color colour = {iteration, iteration, iteration, 255};
-    return colour;
+    return iteration;
 }
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
     /* Create the window */
-        if (!SDL_CreateWindowAndRenderer("Fractals", X_RESOLUTION, Y_RESOLUTION, 0, &window, &renderer)) {
-            SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
-            return SDL_APP_FAILURE;
-        }
+    if (!SDL_CreateWindowAndRenderer("Fractals", X_RESOLUTION, Y_RESOLUTION, 0, &window, &renderer)) {
+        SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    // Create surface
+    surface = SDL_CreateSurface(X_RESOLUTION, Y_RESOLUTION, SDL_PIXELFORMAT_RGBA8888);
+
     return SDL_APP_CONTINUE;
 }
 
@@ -81,21 +97,37 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 /* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    SDL_Surface* surface = SDL_CreateSurface(X_RESOLUTION, Y_RESOLUTION, SDL_PIXELFORMAT_RGBA8888);
+    // Only render new frame if zoom changes - saves a lot of performance because my code is slow
+    if (zoom != last_zoom) {
+        int iteration_counts[X_RESOLUTION][Y_RESOLUTION] = {};
+        int num_iteration_per_pixel[255] = {};
 
-    SDL_LockSurface(surface);
-    for (int x = 0; x < X_RESOLUTION; x++) {
-        for (int y = 0; y < Y_RESOLUTION; y++) {
-            SDL_Color colour = mandelbrot(x, y);
-            SDL_WriteSurfacePixel(surface, x, y, colour.r, colour.g, colour.b, 255);
+        SDL_LockSurface(surface);
+        for (int x = 0; x < X_RESOLUTION; x++) {
+            for (int y = 0; y < Y_RESOLUTION; y++) {
+                int iteration = Mandelbrot(x, y);
+                //iteration_counts[x][y] = iteration;
+                //num_iteration_per_pixel[iteration]++;
+
+                SDL_WriteSurfacePixel(surface, x, y, 0, 0, iteration / 2, 255);
+            }
         }
+
+        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+        SDL_RenderTexture(renderer, texture, NULL,NULL);
+        SDL_RenderPresent(renderer);
+
+        SDL_UnlockSurface(surface);
+        SDL_DestroyTexture(texture);
+        last_zoom = zoom;
     }
+    else {
+        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+        SDL_RenderTexture(renderer, texture, NULL,NULL);
+        SDL_RenderPresent(renderer);
 
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_RenderTexture(renderer, texture, NULL,NULL);
-    SDL_RenderPresent(renderer);
-
-    SDL_DestroySurface(surface);
+        SDL_DestroyTexture(texture);
+    }
 
     return SDL_APP_CONTINUE;
 }
